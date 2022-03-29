e804455c0a7d196971718cbd765bbd2e8373375d
Krzysztof Gębka <krzysztof.gebka2001@gmail.com>
2022-03-29 22:52:21 +0200

    Implementation of adding Person to Bill

--- Adliho.Core/src/Bill.java
+++ Adliho.Core/src/Bill.java
@@ -9,4 +9,16 @@ public class Bill
     {
         this.whoAndHowManyPaid = new HashMap<>();
     }
+
+    public void addPersonToBill(String who, Money howManyPaid)
+    {
+        if(who==null)
+            throw new NullPointerException("Peson can not be null");
+        if(howManyPaid==null)
+            throw new NullPointerException("Money can not be null");
+        if(whoAndHowManyPaid.containsKey(who))
+            throw new IllegalStateException("This person is already exist in Bill");
+
+        whoAndHowManyPaid.put(who, howManyPaid);
+    }
 }
