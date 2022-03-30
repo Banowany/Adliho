db62cfa1cf44d2148462c2ba96b78b3cef4812f5
Krzysztof Gębka <krzysztof.gebka2001@gmail.com>
2022-03-30 18:40:36 +0200

    Adding possibility to add debt collector to Personal Settlement

--- Adliho.Core/src/PersonalSettlement.java
+++ Adliho.Core/src/PersonalSettlement.java
@@ -4,13 +4,25 @@ import java.util.Map;
 public class PersonalSettlement
 {
     String who;
-    Map<String, Money> howManyBackedAndToWhom;
+    Map<String, Money> howManyToBackAndToWhom;
     Money howManyPaid;
 
     public PersonalSettlement(String who, Money howManyPaid)
     {
         this.who = who;
-        this.howManyBackedAndToWhom = new HashMap<>();
+        this.howManyToBackAndToWhom = new HashMap<>();
         this.howManyPaid = howManyPaid;
+    }
+
+    public void addCollector(String name, Money howManyToBack)
+    {
+        if(name==null)
+            throw new NullPointerException("Collector can not be null");
+        if(howManyToBack==null)
+            throw new NullPointerException("Money can not be null");
+        if(howManyToBackAndToWhom.containsKey(name))
+            throw new IllegalStateException("This collector is already exist in PersonalSettlement.");
+
+        howManyToBackAndToWhom.put(name, howManyToBack);
     }
 }
