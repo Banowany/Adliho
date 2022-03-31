74be1ce9c9ff076feea301f82193f5c4f717a660
Krzysztof Gębka <krzysztof.gebka2001@gmail.com>
2022-03-31 06:36:44 +0200

    Implementation of Joint Settlement

---
+++ Adliho.Core/src/JointSettlement.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class JointSettlement
+{
+    List<PersonalSettlement> debtors;
+
+    public JointSettlement()
+    {
+        this.debtors = new ArrayList<>();
+    }
+}
